02d698dc0b29f9d4543e43f76087534203737789
InventoryServer <explosive.inventory@gmail.com>
2010-07-30 21:56:51 +0500

    adding is_approved to session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -21,6 +21,7 @@ class SessionsController < ApplicationController
       session[:user_name] = user.login
       session[:user_id] = user.id
       session[:role] = user.role
+      session[:is_approved] = user.is_approved
       redirect_back_or_default('/')
       flash[:notice] = "Logged in successfully"
     else
